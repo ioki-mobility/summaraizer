fb2c1c819d135888fb67f5b3b595a64bcc654669
StefMa <StefMaDev@outlook.com>
2024-05-14 09:59:03 +0200

    Add support for OpenAI

--- provider/openai.go
+++ provider/openai.go
@@ -1,7 +1,10 @@
 package provider
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
 
 	"github.com/ioki-mobility/summaraizer"
 )
@@ -15,5 +18,72 @@ type OpenAi struct {
 func (o *OpenAi) Summarize(
 	comments summaraizer.Comments,
 ) (string, error) {
-	return fmt.Sprintf("This is a summary from OpenAI with model %s", o.Model), nil
+	prompt, err := resolvePrompt(o.Common.Prompt, comments)
+	if err != nil {
+		return "", err
+	}
+
+	request := openAiRequest{
+		Model: o.Model,
+		Messages: []openAiRequestMessage{
+			{
+				Role:    "user",
+				Content: prompt,
+			},
+		},
+	}
+	reqBodyBytes, err := json.Marshal(request)
+	if err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(
+		"POST",
+		"https://api.openai.com/v1/chat/completions",
+		bytes.NewBuffer(reqBodyBytes),
+	)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("Authorization", "Bearer "+o.ApiToken)
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var response openAiResponse
+	err = json.Unmarshal(respBody, &response)
+	if err != nil {
+		return "", err
+	}
+
+	return response.Choices[0].Message.Content, nil
+}
+
+type openAiRequest struct {
+	Model    string                 `json:"model"`
+	Messages []openAiRequestMessage `json:"messages"`
+}
+
+type openAiRequestMessage struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
+
+type openAiResponse struct {
+	Choices []struct {
+		Message struct {
+			Content string `json:"content"`
+			Role    string `json:"role"`
+		} `json:"message"`
+	} `json:"choices"`
 }
